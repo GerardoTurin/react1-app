73481c7cc8a7bbeeba9bfe656df271514a9af544
Gerardo <gera_1993_06@hotmail.com>
2022-07-03 20:04:15 -0300

    Updates

---
+++ precache-manifest.f76561bb6146fffb7a5aa2231f84ba72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "70291919182738ccd016",
+    "url": "/react1-app/static/css/main.8a70f4c6.chunk.css"
+  },
+  {
+    "revision": "70291919182738ccd016",
+    "url": "/react1-app/static/js/main.70291919.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/react1-app/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "75ede5155ef10b3c6576",
+    "url": "/react1-app/static/js/runtime~main.75ede515.js"
+  },
+  {
+    "revision": "ec6318e89bd5de45e531f3dac9281fce",
+    "url": "/react1-app/static/media/testimonio-Emma.ec6318e8.png"
+  },
+  {
+    "revision": "30aa65c477fbbb29dd8783fd97fe0f84",
+    "url": "/react1-app/static/media/testimonio-Sarah.30aa65c4.png"
+  },
+  {
+    "revision": "788e28f720a4e1e00d3292f95fddc594",
+    "url": "/react1-app/static/media/testimonio-Shawn.788e28f7.png"
+  },
+  {
+    "revision": "956ce5a6ea99235debb71a34408e1aa6",
+    "url": "/react1-app/index.html"
+  }
+];
